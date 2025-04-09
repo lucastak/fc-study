53a12f39eb6735ed175b98343f7c54d9da174a0a
lucastak <contato.lucas.santos@hotmail.com>
2025-04-09 17:08:01 -0300

    adiciona exemplo de agregado

---
+++ ddd/src/main.ts
@@ -0,0 +1,16 @@
+import Address from "./entity/address";
+import Customer from "./entity/customer";
+import Order from "./entity/order";
+import OrderItem from "./entity/order_item";
+
+//relacao - ID
+let customer = new Customer("1", "John Doe");
+const address = new Address("Street 1", 123, "123453-123", "City");
+customer.Address = address;
+customer.activate();
+
+
+//eelacao Objeto - Entidade
+const item1 = new OrderItem("1", "Item 1", 100);
+const item2 = new OrderItem("2", "Item 2", 200);
+const order = new Order("1", customer.id, [item1, item2]);
