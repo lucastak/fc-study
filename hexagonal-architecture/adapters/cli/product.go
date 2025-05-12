4b4e17e2faef684b94990caa25c0e490c822bd19
lucastak <contato.lucas.santos@hotmail.com>
2025-05-12 18:54:19 -0300

    feat: cria product cli

---
+++ hexagonal-architecture/adapters/cli/product.go
@@ -0,0 +1,48 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/lucastak/go-hexagonal/application"
+)
+
+func Run(service application.ProductServiceInterface, action string, productId string, productName string, price float64) (string, error) {
+	var result = ""
+
+	switch action {
+	case "create":
+		product, err := service.Create(productName, price)
+		if err != nil {
+			return result, err
+		}
+		result = fmt.Sprintf("Product ID %s with the name %s has been created with price %f and status %s", product.GetID(), product.GetName(), product.GetPrice(), product.GetStatus())
+	case "enabled":
+		product, err := service.Get(productId)
+		if err != nil {
+			return result, err
+		}
+		res, err := service.Enabled(product)
+		if err != nil {
+			return result, err
+		}
+		result = fmt.Sprintf("Product %s has been enabled", res.GetName())
+	case "disabled":
+		product, err := service.Get(productId)
+		if err != nil {
+			return result, err
+		}
+		res, err := service.Disabled(product)
+		if err != nil {
+			return result, err
+		}
+		result = fmt.Sprintf("Product %s has been disabled", res.GetName())
+	default:
+		product, err := service.Get(productId)
+		if err != nil {
+			return result, err
+		}
+		result = fmt.Sprintf("Product ID %s with the name %s has been created with price %f and status %s", product.GetID(), product.GetName(), product.GetPrice(), product.GetStatus())
+	}
+
+	return result, nil
+}
