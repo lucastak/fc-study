fac2ff34b574edfee4069ad4fcd2e32f38cd0166
lucastak <contato.lucas.santos@hotmail.com>
2025-05-27 19:32:16 -0300

    teste unitario para criacao do user

--- clean-arq/src/usecase/customer/create/create.customer.unit.spec.ts
+++ clean-arq/src/usecase/customer/create/create.customer.unit.spec.ts
@@ -36,4 +36,20 @@ describe("Unit test create customer use case", () => {
             }
         });
     });
+
+    it("should throw an error when name is missing", async () => {
+        const customerRepository = MockRepository();
+        const useCase = new CreateCustomerUseCase(customerRepository);
+        input.name = "";
+
+        await expect(useCase.execute(input)).rejects.toThrow("Name is required");
+    })
+
+    it("should throw an error when street is missing", async () => {
+        const customerRepository = MockRepository();
+        const useCase = new CreateCustomerUseCase(customerRepository);
+        input.address.street = "";
+
+        await expect(useCase.execute(input)).rejects.toThrow("Street is required");
+    });
 });
