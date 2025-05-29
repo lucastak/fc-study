271b0e98ff8edd79040800505657b481cf3692a3
lucastak <contato.lucas.santos@hotmail.com>
2025-05-29 20:01:06 -0300

    adiciona teste e2e da camada de api

--- clean-arq/src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ clean-arq/src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -1,13 +1,13 @@
-import {app, sequilize} from "../express";
+import {app, sequelize} from "../express";
 import request from "supertest";
 
 
 describe("E2E test for customer", () => {
     beforeEach(async () => {
-        await sequilize.sync({ force: true });
+        await sequelize.sync({ force: true });
     });
     afterAll(async () => {
-        await sequilize.close();
+        await sequelize.close();
     });
 
     it("should create a customer", async () => {
