95067aa01c912b861ca94fe59a50eca68aa2d24e
lucastak <contato.lucas.santos@hotmail.com>
2025-07-26 12:36:40 -0300

    feat: implementa product add e find

--- monolithic-application/src/modules/@shared/domain/value-object/value-object.interface.ts
+++ monolithic-application/src/modules/@shared/domain/value-object/value-object.interface.ts
@@ -1,3 +1,1 @@
-export default interface ValueObject{
-    
-}
+export default interface ValueObject {}
--- monolithic-application/src/modules/product-adm/domain/product.entity.ts
+++ monolithic-application/src/modules/product-adm/domain/product.entity.ts
@@ -1,60 +1,60 @@
-import { AggregateRoot } from "../../@shared/domain/entity/aggregate-root.interface";
 import BaseEntity from "../../@shared/domain/entity/base.entity";
+import AggregateRoot from "../../@shared/domain/entity/aggregate-root.interface";
 import Id from "../../@shared/domain/value-object/id.value-object";
 
 type ProductProps = {
-    id?: Id;
-    name: string;
-    description: string;
-    purchasePrice: number;
-    stock: number;
-    createdAt?: Date;
-    updatedAt?: Date;
-}
+  id?: Id;
+  name: string;
+  description: string;
+  purchasePrice: number;
+  stock: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
 
 export default class Product extends BaseEntity implements AggregateRoot {
-    private _name: string;
-    private _purchasePrice: number;
-    private _stock: number;
-    private _description: string;
+  private _name: string;
+  private _description: string;
+  private _purchasePrice: number;
+  private _stock: number;
 
-    constructor(props: ProductProps) {
-        super(props.id);
-        this._name = props.name;
-        this._purchasePrice = props.purchasePrice;
-        this._stock = props.stock;
-        this._description = props.description;
-    }
+  constructor(props: ProductProps) {
+    super(props.id);
+    this._name = props.name;
+    this._description = props.description;
+    this._purchasePrice = props.purchasePrice;
+    this._stock = props.stock;
+  }
 
-    get name(): string {
-        return this._name;
-    }
+  get name(): string {
+    return this._name;
+  }
 
-    get purchasePrice(): number {
-        return this._purchasePrice;
-    }
+  get description(): string {
+    return this._description;
+  }
 
-    get stock(): number {
-        return this._stock;
-    }
+  get purchasePrice(): number {
+    return this._purchasePrice;
+  }
 
-    get description(): string {
-        return this._description;
-    }
+  get stock(): number {
+    return this._stock;
+  }
 
-    set stock(value: number) {
-        this._stock = value;
-    }
+  set name(name: string) {
+    this._name = name;
+  }
 
-    set description(value: string) {
-        this._description = value;
-    }
+  set stock(stock: number) {
+    this._stock = stock;
+  }
 
-    set name(value: string) {
-        this._name = value;
-    }
+  set description(description: string) {
+    this._description = description;
+  }
 
-    set purchasePrice(value: number) {
-        this._purchasePrice = value;
-    }
+  set purchasePrice(purchasePrice: number) {
+    this._purchasePrice = purchasePrice;
+  }
 }
